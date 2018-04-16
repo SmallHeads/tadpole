6b4e85a2317beac63af055995483500578b83380
Andrew Doyle <andrew.doyle@mcgill.ca>
2018-04-16 17:35:23 -0400

    bug

--- train.py
+++ train.py
@@ -154,6 +154,10 @@ if __name__ == "__main__":
         adas_train, adas_test = y_t_train.iloc[:, 1], y_t_test.iloc[:, 1]
         ventricle_train, ventricle_test = y_t_train.iloc[:, 2], y_t_test.iloc[:, 2]
 
+        print(dx_train)
+
+        
+
 
         model = mlp(x_t_train.shape[-1])
         model.summary()
